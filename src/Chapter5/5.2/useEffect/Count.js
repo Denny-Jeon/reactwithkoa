5cd13ff49d8787a6fb48ccdcec3c58b0bb74b230
Denny-Jeon <futureds@naver.com>
2019-12-20 14:08:25 +0900

    useEffect 기능 추가

--- src/Chapter5/5.2/useEffect/Count.js
+++ src/Chapter5/5.2/useEffect/Count.js
@@ -1,49 +1,41 @@
 // Count.js
-import React, { useState, useReducer, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
+// count를 출력한 컴포넌트 import
+import CountView from "./CountView";
+// count를 증가시킬 컴포넌트 import
+import HandleView from "./HandleView";
 
 const Count = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { count, step } = state;
+  // useState을 이용해 count를 관리한다.
+  const [count, setCount] = useState(0);
+  const [evenOrOdd, setEvenOrOdd] = useState(true);
+
+  console.log("abc", count, evenOrOdd);
 
   useEffect(() => {
-    const id = setInterval(() => {
-      dispatch({ type: "tick" });
-    }, 1000);
-    return () => clearInterval(id);
-  }, [dispatch]);
+    console.log(123);
+
+    if (count % 2 === 0) {
+      setEvenOrOdd(true);
+    } else {
+      setEvenOrOdd(false);
+    }
+  });
 
   return (
-    <>
-      <h1>{count}</h1>
-      <input
-                value={step}
-                onChange={(e) => {
-          dispatch({
-            type: "step",
-            step: Number(e.target.value),
-          });
-        }}
-      />
-    </>
+    <div>
+      <div> Hello, JSX</div>
+      {/* count를 props으로 전송 */}
+      <CountView count={count} />
+      {/* handleIncrease props으로 전송 */}
+      <HandleView handleIncrease={() => setCount((prevCount) => prevCount + 1)} />
+
+      <div>
+        {evenOrOdd ? "짝수" : "홀수"}
+      </div>
+    </div>
   );
 };
-
-
-const initialState = {
-  count: 0,
-  step: 1,
-};
-
-function reducer(state, action) {
-  const { count, step } = state;
-  if (action.type === 'tick') {
-    return { count: count + step, step };
-  } if (action.type === 'step') {
-    return { count, step: action.step };
-  } 
-    throw new Error();
-  
-}
 
 export default Count;
