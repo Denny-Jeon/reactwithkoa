70a811d0b61a39663036c5073bdcbe6c01e37c47
Denny-Jeon <futureds@naver.com>
2019-12-26 16:19:20 +0900

    Change package.json
    babel-node 제거

--- koa/src/index.js
+++ koa/src/index.js
@@ -3,6 +3,14 @@ const Koa = require("koa");
 const app = new Koa();
 const port = 3002;
 
+app.use(async ctx => {
+    console.log("First");
+})
+
+app.use(ctx => {
+    console.log("Second");
+})
+
 app.use(async (ctx) => {
     ctx.body = "Hello World";
 });
