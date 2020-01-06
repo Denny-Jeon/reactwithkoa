599c2f50a58c7f32e831be0a243b8f72de86494e
Denny-Jeon <futureds@naver.com>
2020-01-06 16:09:51 +0900

    Add babel and middleware
    Babel 기능 추가 (.babelrc)
    Babel  플러그인 추가
     babel-plugin-module-resolver
     babel-plugin-transform-object-rest-spread
     babel-preset-env
    Koa Middleware 기능 추가

--- koa/src/index.js
+++ koa/src/index.js
@@ -1,22 +1,23 @@
-const Koa = require("koa");
+import Koa from "koa";
 
 const app = new Koa();
 const port = 3002;
 
-app.use(async (ctx) => {
-    console.log("First");
-});
-
-app.use((ctx) => {
-    console.log("Second");
-});
 
 app.use(async (ctx) => {
     ctx.body = "Hello World";
 });
 
-app.use(async (ctx) => {
-    ctx.body = "fasdfsadfasdf";
+// 로그 미들웨어 생성
+app.use(async (ctx, next) => {
+    const requestTime = Date.now();
+
+    // 하위 Downstream 미들웨어 수행 대기
+    await next();
+
+    // Downstream 미들웨어 수행 완료 후 로그 기록
+    const responseTime = Date.now();
+    console.log(`${ctx.method} ${ctx.url} ${ctx.status}, ${responseTime - requestTime}ms`);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
