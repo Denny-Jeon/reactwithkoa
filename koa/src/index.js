29f88d59801abcac3629eea6befaac44854879ab
Denny-Jeon <futureds@naver.com>
2020-01-09 09:42:20 +0900

    Modify index
    throw, assert 기능 추가

--- koa/src/index.js
+++ koa/src/index.js
@@ -11,12 +11,14 @@ app.on("log", (ctx, ms) => {
 
 // 로그 미들웨어 생성
 app.use(async (ctx, next) => {
+    ctx.respond = false;
     const requestTime = Date.now();
     // 최초 요청시에는 쿠키가 없고, 두번째 요청부터 쿠키 값이 출력된다.
     console.log(ctx.cookies.get("testCookie"));
 
     // 하위 Downstream 미들웨어 수행 대기
     await next();
+
 
     // Downstream 미들웨어 수행 완료 후 로그 기록
     const responseTime = Date.now();
@@ -35,6 +37,11 @@ app.use(async (ctx) => {
             expires: new Date((1000 * 10) + Date.now()),
         },
     );
+
+    const errorProperties = {
+        type: "System error",
+    };
+    ctx.assert(errorProperties.type !== "System error", 400, "Hello World Error", errorProperties);
 
     ctx.body = "Hello world";
 });
