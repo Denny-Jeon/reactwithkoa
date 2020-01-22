9a9131482e9e69ddc69fec2a98e1f46d832aa887
Denny-Jeon <futureds@naver.com>
2020-01-22 14:38:56 +0900

    Modify ReactHookForm
    Add sleep 기능 추가
    Add schema의 패스워드에 test 기능 추가

--- client/src/Chapter5/ReactHookForm/ReactHookForm.js
+++ client/src/Chapter5/ReactHookForm/ReactHookForm.js
@@ -1,16 +1,55 @@
 import React from "react";
-// ErrorMessage 추가
 import { useForm, ErrorMessage } from "react-hook-form";
+import * as Yup from "yup";
+
+const defaultValues = {
+  email: "",
+  password: "",
+  gender: "male",
+  sns: {
+    facebook: false,
+    google: false,
+    twitter: false,
+  },
+  age: 10,
+};
+
+const schema = Yup.object().shape({
+  email: Yup.string().email("Invalid email address").required("Required"),
+  password: Yup.string()
+    .min(8, "password length > 8")
+    .required("Required")
+    .test("not permit 12345678", "not permit 12345678", (value) => value !== "12345678"),
+  sns: Yup.object().shape({
+    facebook: Yup.bool(),
+    google: Yup.bool(),
+    twitter: Yup.bool(),
+  }).test("Select at least one", "Select at least one", (value) => value.facebook || value.google || value.twitter),
+  age: Yup.number().min(1, "age > 1 ").required("Required"),
+});
+
 
 const ReactHookForm = () => {
-  // errors 출력을 위한 errors 객체 추가
+  // touched, isValid, isSubmitting을 제어하기 위한 폼 상태 값 formState 추출
   const {
-    register, handleSubmit, reset, errors,
-  } = useForm();
+    register, handleSubmit, reset, errors, formState,
+  } = useForm({
+    mode: "onBlur", // 유효성 검증을 input을 벗어날 때마다 실시, default는 onChange
+    defaultValues,
+    validationSchema: schema,
+  });
 
-  const onSubmit = (data, e) => {
+  const sleep = (second) => new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("foo");
+    }, 1000 * second);
+  });
+
+  const onSubmit = async (data, e) => {
     console.log("Submit event", e);
     console.log(data);
+
+    await sleep(3);
   };
 
   console.log(errors);
@@ -18,44 +57,40 @@ const ReactHookForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>email:
-        <input
-          type="email"
-          placeholder="email"
-          name="email"
-          // register에 required, pattern 유효성 검사 추가
-          ref={register({
-            required: "This is required",
-            pattern: { value: /^\S+@\S+$/i, message: "not email" },
-          })}
-        />
+        <input type="email" placeholder="email" name="email" ref={register} />
       </div>
-      <div>
-        {/* 에러가 발생한 경우 출력 */}
-        <ErrorMessage errors={errors} name="email" />
-        {errors.email && <p>{errors.email.message}</p>}
-      </div>
+      {errors.email && <p>{errors.email.message}</p>}
       <div>password:
-        <input
-          type="password"
-          placeholder="password"
-          name="password"
-          ref={register({
-            required: true,
-            minLength: 8,
-            validate: ((value) => value !== "12345678"),
-          })}
-        />
+        <input type="password" placeholder="password" name="password" ref={register} />
       </div>
-      <div>
-        {/* 에러가 발생한 경우 출력 */}
-        {errors.password && errors.password.type === "required" && <p>required</p>}
-        {errors.password && errors.password.type === "minLength" && <p>minLength</p>}
-        {errors.password && errors.password.type === "validate" && <p>not permit 12345678</p>}
-      </div>
+      { (errors.password) && (<div>{errors.password.message}</div>)}
 
+      <div>gender:
+        <input name="gender" type="radio" value="male" ref={register} /> male
+        <input name="gender" type="radio" value="female" ref={register} /> female
+      </div>
       <div>
-        <button type="submit">제출</button>
-        <button type="button" onClick={() => reset()}>취소</button>
+        Sns:
+        <input type="checkbox" placeholder="sns.google" name="sns.google" ref={register} /> google
+        <input type="checkbox" placeholder="sns.facebook" name="sns.facebook" ref={register} /> facebook
+        <input type="checkbox" placeholder="sns.twitter" name="sns.twitter" ref={register} /> twitter
+      </div>
+      { (errors.sns) && (<div>{errors.sns.message}</div>)}
+      <div>
+          Age:
+        <select name="age" ref={register}>
+          <option value={0}>-------------</option>
+          {
+            [...Array(100).keys()].map((x) => <option key={x} value={x + 1}>{x + 1}</option>)
+          }
+        </select>
+      </div>
+      <ErrorMessage errors={errors} name="age" />
+      { (errors.age) && (<div>{errors.age.message}</div>)}
+      <div>
+        {/* 폼 상태가 isValid 하거나 isSubmitting인 아닌 경우 활성화 */}
+        <button type="submit" disabled={!formState.isValid || formState.isSubmitting}>제출</button>
+        <button type="button" onClick={() => reset()}>Reset</button>
       </div>
     </form>
   );
