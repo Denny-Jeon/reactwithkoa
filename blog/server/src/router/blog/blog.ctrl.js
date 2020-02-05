7a3c9ae4004823f908eea565054b3b4a9654c54b
Denny-Jeon <futureds@naver.com>
2020-02-05 13:47:27 +0900

    Modify blog.ctrl
    Blog 테이블 조회, 수정, 삭제를 위한 getOne, put, remove 함수 추가

--- blog/server/src/router/blog/blog.ctrl.js
+++ blog/server/src/router/blog/blog.ctrl.js
@@ -5,7 +5,6 @@ import Moment from "moment";
 export const post = async (ctx) => {
     const { title, content } = ctx.request.body;
     const createdAt = Moment().format("YYYY-MM-DD HH:mm:ss");
-    // const db = Database.getDb();
 
     const statement = await ctx.state.db.run(
         SQL`INSERT INTO 
@@ -19,11 +18,58 @@ export const post = async (ctx) => {
 
 
 export const get = async (ctx) => {
-    // const db = Database.getDb();
-
     const statement = await ctx.state.db.all(SQL`SELECT * FROM Blog`);
     if (!statement) throw Boom.badRequest("bad Request");
 
     ctx.status = 200;
     ctx.body = statement;
+};
+
+
+export const getOne = async (ctx) => {
+    const { id } = ctx.params;
+
+    const statement = await ctx.state.db.get(
+        SQL`SELECT id, title, content, createdAt 
+            FROM Blog
+            WHERE id=${id}`,
+    );
+    if (!statement) throw Boom.badRequest("bad Request");
+
+    ctx.status = 200;
+    ctx.body = statement;
+};
+
+export const put = async (ctx) => {
+    const { id } = ctx.params;
+    const { title, content } = ctx.request.body;
+
+    const statement = await ctx.state.db.run(
+        SQL`UPDATE Blog 
+            SET title=${title}, content=${content}
+            WHERE id=${id}`,
+    );
+
+    if (!statement) throw Boom.badRequest("bad Request");
+    if (statement && statement.changes === 0) {
+        throw Boom.notFound("not Found");
+    }
+
+    ctx.status = 200;
+};
+
+export const remove = async (ctx) => {
+    const { id } = ctx.params;
+
+    const statement = await ctx.state.db.run(
+        SQL`DELETE FROM Blog
+            WHERE id=${id}`,
+    );
+
+    if (!statement) throw Boom.badRequest("bad Request");
+    if (statement && statement.changes === 0) {
+        throw Boom.notFound("not Found");
+    }
+
+    ctx.status = 204;
 };
--- blog/server/src/router/blog/index.js
+++ blog/server/src/router/blog/index.js
@@ -5,6 +5,8 @@ const router = new KoaRouter();
 
 router.post("/", Blog.post);
 router.get("/", Blog.get);
-
+router.get("/:id", Blog.getOne);
+router.put("/:id", Blog.put);
+router.delete("/:id", Blog.remove);
 
 export default router;
