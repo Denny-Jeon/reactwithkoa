09719c8317e10b6b2717d832d56aacc3ffddfa19
Denny-Jeon <futureds@naver.com>
2020-02-20 14:12:46 +0900

    Modify footer
    footer에 AdminLTE3 적용

--- blog/client/src/pages/Layout/Footer.js
+++ blog/client/src/pages/Layout/Footer.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { StyleWrapper } from "../../components";
 
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+  <>
+    <StyleWrapper
+      style={{
+        paddingBottom: "80px",
+      }}
+    />
+    <footer className="main-footer fixed-bottom">
+      <div className="float-right d-none d-sm-inline">
+        Blog
+      </div>
+      <strong>Copyright © 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    </footer>
+  </>
 );
 
 export default Footer;
